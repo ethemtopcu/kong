edb3feb6a1446da83e1e1052a406f08c6af52569
İbrahim Ethem Topçu <64851451+ethemtopcu@users.noreply.github.com>
2020-07-06 15:07:43 +0300

    Add sum function without tests (#1)
    * Add sum function without tests
    
    * Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
-// module.exports = {
-//     setupFilesAfterEnv: ["./jest.setup.js"]
-// };
+module.exports = {
+    coverageThreshold: {
+      global: {
+        statements: 98,
+        branches: 91,
+        functions: 98,
+        lines: 98,
+      },
+    },
+    coverageDirectory: 'coverage',
+    collectCoverage: true,
+    collectCoverageFrom: ['./*.js', '!jest.config.js', 'src/*.js', 'src/**/*.js', '!src/**/*.test.js', '!src/index.js'],
+    coverageReporters: ['json', 'text', 'lcov'],
+    moduleDirectories: ['node_modules'],
+    testRegex: '.*\\.test\\.js$',
+    // testResultsProcessor: './resultsProcessor',
+  };
+  
